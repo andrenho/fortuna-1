33f4c6aa985bd63de258da88ad52a47c210a0dfd
André Wagner <WagnerAndre@JohnDeere.com>
2021-02-24 02:13:37 +0000

    .

--- firmware/controller/repl.cc
+++ firmware/controller/repl.cc
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include <pb_decode.h>
+#include <pb_encode.h>
 #include "messages.pb.h"
 #include "common/protocol.h"
 #include "serial.hh"
@@ -20,34 +21,74 @@ static unsigned int free_ram()
     return free;
 }
 
+static Reply parse_response(Request* request)
+{
+    Reply reply;
+    reply.type = MessageType_FREE_MEM;
+    reply.result = Result_OK;
+    reply.payload.free_mem = 1234;
+    return reply;
+}
+
 static void repl_do_protobuf()
 {
     // get message size
     uint16_t msg_sz = serial.recv16();
     if (msg_sz > MAX_MSG_SZ) {
         serial.send(Z_REQUEST_TOO_LARGE);
+        return;
     }
+
+    struct Data {
+        Serial* serial;
+        int     bytes;
+    };
 
     // receive message
     Request request = Request_init_zero;
-    pb_istream_t stream = {
-        [](pb_istream_t *stream, uint8_t *buf, size_t count) -> bool {
-            Serial* serial = static_cast<Serial*>(stream->state);
-            for (size_t i = 0; i < count; ++i)
-                buf[i] = serial->recv();
+    Data data = { &serial, msg_sz };
+    pb_istream_t istream = {
+        [](pb_istream_t* stream, uint8_t *buf, size_t count) -> bool {
+            Data* data = static_cast<Data*>(stream->state);
+            for (size_t i = 0; i < count; ++i) {
+                buf[i] = data->serial->recv();
+                --data->bytes;
+                if (data->bytes == 0) {
+                    return false;
+                }
+            }
             return true;
         },
         &serial,
         MAX_MSG_SZ,
-        NULL
+        nullptr
     };
-    pb_decode(&stream, Request_fields, &request);
+    pb_decode(&istream, Request_fields, &request);
 
-    // calculate checksum
-    serial.recv();  // TODO
+    // calculate checksum (TODO)
+    serial.recv();
     serial.recv();
     
+    // reply
+    Reply reply = parse_response(&request);
     serial.send(Z_FOLLOWS_PROTOBUF_RESP);
+    pb_ostream_t ostream = {
+        [](pb_ostream_t* stream, const uint8_t* buf, size_t count) -> bool {
+            Serial* serial = static_cast<Serial*>(stream->state);
+            for (size_t i = 0; i < count; ++i)
+                serial->send(buf[i]);
+            return true;
+        },
+        &serial,
+        MAX_MSG_SZ,
+        0,
+        nullptr
+    };
+    pb_encode(&ostream, Reply_fields, &reply);
+
+    // send checksum (TODO)
+    serial.send(0xff);
+    serial.send(0xff);
 }
 
 void repl_do()
