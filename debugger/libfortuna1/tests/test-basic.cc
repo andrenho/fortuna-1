6db1c1eb30f91896032a302f6ceb4ed7c213936e
André Wagner <WagnerAndre@JohnDeere.com>
2021-02-25 08:08:24 -0300

    .

--- debugger/libfortuna1/tests/test-basic.cc
+++ debugger/libfortuna1/tests/test-basic.cc
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
     TestArgs test_args(argc, argv);
     auto f = test_args.create_fortuna();
     
-    ASSERT_GT("Test memory amount", f->free_mem(), 0);
+    ASSERT_GT("Test memory amount", f->free_mem(), 0UL);
     
     std::cout << "Testing debug messages...\n";
     f->test_debug_messages();
--- debugger/libfortuna1/tests/tsupport.hh
+++ debugger/libfortuna1/tests/tsupport.hh
@@ -18,12 +18,18 @@ private:
     static void show_help(const char* program) ;
 };
 
-#define ASSERT_GT(msg, value, greater_than) \
+#define ASSERT_GT(msg, value, greater_than) { \
     std::cout << msg << "... "; \
-    if (value > greater_than) { std::cout << "\e[0;32m✔\e[0m\n"; } else { std::cout << "\e[0;31mX\e[0m   Expected received value of " << value << " to be > " << greater_than << ".\n"; exit(1); }
+    auto __v = value;                       \
+    auto __g = greater_than;                 \
+    if (__v > __g) { std::cout << "\e[0;32m✔\e[0m\n"; } else { std::cout << "\e[0;31mX\e[0m   Expected received value of " << __v << " to be > " << __g << "\n"; exit(1); } \
+}
 
-#define ASSERT_EQ(msg, expected, received) \
-    std::cout << msg << "... "; \
-    if (expected == received) { std::cout << "\e[0;32m✔\e[0m\n"; } else { std::cout << "\e[0;31mX\e[0m   Expected: " << expected << "  Received: " << received << ".\n"; exit(1); }
+#define ASSERT_EQ(msg, expected, received) { \
+    std::cout << msg << "... ";              \
+    auto __e = expected;                       \
+    auto __r = received;                       \
+    if (__e == __r) { std::cout << "\e[0;32m✔\e[0m\n"; } else { std::cout << "\e[0;31mX\e[0m   Expected: " << __e << "  Received: " << __r << "\n"; exit(1); } \
+}
 
 #endif //LIBFORTUNA1_TSUPPORT_HH
