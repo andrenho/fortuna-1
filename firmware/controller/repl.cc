d3ac2578ec9a3368048c19d6ba3f89765e7afa12
André Wagner <WagnerAndre@JohnDeere.com>
2021-03-02 22:55:08 -0300

    .

--- firmware/controller/repl.cc
+++ firmware/controller/repl.cc
@@ -85,13 +85,14 @@ void Repl::do_terminal(char cmd)
         case 'd': {
                 int page = ask_value_P(PSTR("Page (0x100)"));
                 if (page != ERROR) {
-                    if (!ram_.read_block(page * 0x100, 0x100, [](uint16_t addr, uint8_t byte, void*) {
+                    // if (!ram_.write_block(page * 0x100, 0x100, [](uint16_t idx)
+                    if (!ram_.read_block(page * 0x100, 0x100, [](uint16_t addr, uint8_t byte, void* page) {
                         if (addr % 0x10 == 0)
-                            printf_P(PSTR("%04X : "), addr);
+                            printf_P(PSTR("%04X : "), addr + (*(int*)page * 0x100));
                         printf_P(PSTR("%02X "), byte);
                         if (addr % 0x10 == 0xf)
                             putchar('\n');
-                    }))
+                    }, &page))
                         printf_P(PSTR("Checksum error\n"));
                 }
             }
